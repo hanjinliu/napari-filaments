7fbb4eee3286e088a8df44ff213a39d4cdcf6455
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2022-06-21 12:11:46 +0900

    parametrize

--- src/napari_filaments/_widget.py
+++ src/napari_filaments/_widget.py
@@ -1,5 +1,6 @@
+from functools import wraps
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Callable, TypeVar, Union
 
 import numpy as np
 from magicclass import (
@@ -15,13 +16,48 @@ from napari.layers import Image
 from ._spline import Spline
 
 if TYPE_CHECKING:
-    ...
+    from typing_extensions import ParamSpec
+
+    _P = ParamSpec("_P")
+    _R = TypeVar("_R")
+
+
+def batch(f: "Callable[_P, _R]") -> "Callable[_P, _R]":
+    @wraps(f)
+    def _f(self, *args, **kwargs):
+        if args:
+            idx = args[0]
+            _args = args[1:]
+        else:
+            idx = kwargs["idx"]
+            _args = ()
+        if isinstance(idx, (set, list, tuple)):
+            for i in idx:
+                f(self, i, *_args, **kwargs)
+            return
+        f(self, idx, *_args, **kwargs)
+        return
+
+    return _f
 
 
 @magicclass
 class FilamentAnalyzer(MagicTemplate):
     color_default = vfield(Color, options={"value": "#F8FF69"}, record=False)
     lattice_width = vfield(17, options={"min": 5, "max": 49}, record=False)
+    dx = vfield(5.0, options={"min": 1, "max": 50.0}, record=False)
+
+    def __init__(self):
+        self.layer_image = None
+        self.layer_paths = None
+
+    def _get_idx(self, w=None) -> Union[int, set[int]]:
+        if self.layer_paths is None:
+            return 0
+        sel = self.layer_paths.selected_data
+        if len(sel) == 0:
+            return -1
+        return sel
 
     def open_image(self, path: Path):
         from tifffile import imread
@@ -60,34 +96,41 @@ class FilamentAnalyzer(MagicTemplate):
         self._update_paths(idx, fit)
 
     @bind_key("T")
-    def fit_current(self, width: Bound[lattice_width]):
-        self._fit_i(width, -1)
+    @batch
+    def fit_current(
+        self, idx: Bound[_get_idx] = -1, width: Bound[lattice_width] = 9
+    ):
+        self._fit_i(width, idx)
 
-    def extend_left(self):
-        data: np.ndarray = self.layer_paths.data[-1]
+    @batch
+    def extend_left(self, idx: Bound[_get_idx] = -1, dx: Bound[dx] = 5.0):
+        data: np.ndarray = self.layer_paths.data[idx]
         spl = Spline.fit(data, err=0.0)
-        out = spl.extend_left(5.0)
-        self._update_paths(-1, out)
+        out = spl.extend_left(dx)
+        self._update_paths(idx, out)
 
-    def extend_right(self):
-        data: np.ndarray = self.layer_paths.data[-1]
+    @batch
+    def extend_right(self, idx: Bound[_get_idx], dx: Bound[dx] = 5.0):
+        data: np.ndarray = self.layer_paths.data[idx]
         spl = Spline.fit(data, err=0.0)
-        out = spl.extend_right(5.0)
-        self._update_paths(-1, out)
+        out = spl.extend_right(dx)
+        self._update_paths(idx, out)
 
-    def extend_and_fit_left(self):
-        data: np.ndarray = self.layer_paths.data[-1]
+    @batch
+    def extend_and_fit_left(self, idx: Bound[_get_idx], dx: Bound[dx] = 5.0):
+        data: np.ndarray = self.layer_paths.data[idx]
         img = self.layer_image.data
         spl = Spline.fit(data, err=0.0)
-        fit = spl.extend_filament_left(img, 5, width=11, spline_error=3e-2)
-        self._update_paths(-1, fit)
+        fit = spl.extend_filament_left(img, dx, width=11, spline_error=3e-2)
+        self._update_paths(idx, fit)
 
-    def extend_and_fit_right(self):
-        data: np.ndarray = self.layer_paths.data[-1]
+    @batch
+    def extend_and_fit_right(self, idx: Bound[_get_idx], dx: Bound[dx] = 5.0):
+        data: np.ndarray = self.layer_paths.data[idx]
         img = self.layer_image.data
         spl = Spline.fit(data, err=0.0)
-        fit = spl.extend_filament_right(img, 5, width=11, spline_error=3e-2)
-        self._update_paths(-1, fit)
+        fit = spl.extend_filament_right(img, dx, width=11, spline_error=3e-2)
+        self._update_paths(idx, fit)
 
     def plot_profile(self):
         data: np.ndarray = self.layer_paths.data[-1]
