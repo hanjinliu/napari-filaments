d2750f763efab3b32b40434110da9bdc90af6064
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2022-06-27 14:56:01 +0900

    add random seed

--- src/napari_filaments/_tests/test_optimizer.py
+++ src/napari_filaments/_tests/test_optimizer.py
@@ -14,9 +14,10 @@ from napari_filaments import _optimizer as _opt
     ],
 )
 def test_optimization(optimizer: "type[_opt.Optimizer]", params):
+    rng = np.random.default_rng(1234)
     opt = optimizer(params)
     x = np.arange(21)
-    y = opt.sample(x) + np.random.normal(scale=0.1, size=21)
+    y = opt.sample(x) + rng.normal(scale=0.1, size=21)
     opt_optimized = opt.optimize(y)
     opt_fit = optimizer.fit(y)
     assert_allclose(opt_optimized.params, opt_fit.params, rtol=1e-4, atol=1e-4)
